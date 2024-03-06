abfa089956d0f736fb6631351534e583966c6f0e
glckfndr <obulan65@gmail.com>
2024-03-06 13:27:56 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Message from "./components/Message";
+
+function App(){
+  return <div><Message/></div>
+}
+
+export default App;
---
+++ src/components/Message.tsx
@@ -0,0 +1,5 @@
+function Message(){
+  return <h1>Hello my React Application!</h1>
+}
+
+export default Message;
