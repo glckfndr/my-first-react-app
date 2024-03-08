9b1fc430125a13c9b624cb9cc85ec4d789cda1cd
glckfndr <obulan65@gmail.com>
2024-03-08 21:40:54 +0200

    feat: Add Alert with Button

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,12 +1,15 @@
 import { ReactNode } from "react"
 
 interface Props {
-  children: ReactNode
+  children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({children}: Props) => {
+const Alert = ({children, onClose}: Props) => {
   return (
-    <div className="alert alert-primary">{children}</div>
+    <div className="alert alert-primary alert-dismissible fade show">{children}
+    <button type="button" className="btn-close" onClick={onClose} data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
   )
 }
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { MouseEventHandler, ReactNode } from "react";
+
+interface Props {
+  children: string
+  color: "primary" | "secondary" | "success" | "danger";
+  onClick: () => void;
+}
+const Button = ({ children, color, onClick }: Props) => {
+  return (
+    <button className={"btn btn-" + color} data-bs-dismiss="alert" onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
