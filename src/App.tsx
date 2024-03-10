285c93743153b1e20135f01cb859fa4a0d692573
glckfndr <obulan65@gmail.com>
2024-03-10 10:14:36 +0200

    feat: add useRef with  useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
-    return (<div>
-    <ExpandableText maxChars={10}>
-      {"a".repeat(11)}
-    </ExpandableText>
-  </div>);
+    return <Form/>
 }
 
 export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,49 @@
+import { FormEvent, useRef } from "react";
+
+function Form() {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: "", age: 0 };
+
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (nameRef.current !== null) person.name = nameRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    console.log(person);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name:
+        </label>
+        <input
+          ref={nameRef}
+          id="name"
+          type="text"
+          className="form-control"
+          name="name"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age:{" "}
+        </label>
+        <input
+          ref={ageRef}
+          id="age"
+          type="number"
+          className="form-control"
+          name="age"
+        />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default Form;
