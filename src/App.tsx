559e8b36b4c4d2abd11f102abbb56fb4d84e9bc4
glckfndr <obulan65@gmail.com>
2024-03-10 03:07:04 +0200

    feat: Update array of object inside object

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import Cart from "./components/Cart";
 import NavBar from "./components/NavBar";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: 'Spicy Pepperoni',
-    toppings: ['Mushrooms']
+  const [cart, setCart] = useState({
+    discount: 1,
+    items: [
+      {id: 1, title: 'Product1', quantity: 1},
+      {id: 2, title: 'Product2', quantity: 2}
+    ]
   });
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, 'Cheese' ] });
+    setCart({ ...cart, items: cart.items.map((item) =>{
+      if(item.id == 1) item.quantity += 1;
+      return item;
+      }
+      ) });
   };
 
-  return <button onClick={handleClick}>{pizza.toppings} Click Me</button>;
+  return <button onClick={handleClick}>{cart.items[0].quantity} -- Click Me</button>;
 }
 
 export default App;
