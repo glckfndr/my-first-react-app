9f6dc51d56539668e5ae514673a5bbc9828683b6
glckfndr <obulan65@gmail.com>
2024-03-28 16:20:56 +0200

    feat: add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,33 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     const controller = new AbortController();
-
+    setIsLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setIsLoading(false);
       });
+
     return () => controller.abort();
   }, []);
   return (
     <div>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && (
+        <div className="spinner-border text-danger" role="status"></div>
+      )}
       <ul className={"list-group"}>
         {users.map((user) => (
           <li key={user.id} className={"list-group-item"}>
