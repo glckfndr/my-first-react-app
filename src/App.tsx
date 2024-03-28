69d007da2c8c4bcd715439eaecec7b5ea1a0fe53
glckfndr <obulan65@gmail.com>
2024-03-28 08:23:51 +0200

    feat: demo cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import { useEffect, useState } from "react";
-import ProductList from "./components/ProductList";
-
+const connect = () => {
+  console.log("Connect server!");
+};
+const disconnect = () => {
+  console.log("Disconnect server!");
+};
 function App() {
-  const [category, setCategory] = useState("");
+  useEffect(() => {
+    connect();
 
-  return (
-    <div>
-      <select
-        className="form-select"
-        onChange={(ev) => setCategory(ev.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="HouseHold">HouseHold</option>
-      </select>
-      <ProductList category={category} />
-    </div>
-  );
+    return () => disconnect();
+  }, []);
+  return <div></div>;
 }
 
 export default App;
